e208310a4f9d4fff69851316155987948ed9653b
Italo Aurelio <italoaurelior@gmail.com>
2021-03-20 21:58:33 -0300

    Start popup block

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ changeColor.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
+  chrome.storage.sync.get("coins", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
 }
